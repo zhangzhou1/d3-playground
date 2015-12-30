db8127006866af7fcf98a2cbebc17be20dcb72bb
Sam Liu <ontouchstart@gmail.com>
2015-12-29 23:11:29 -0500

    Update and rename dsfs.js to d3fs.js

--- safedata/d3fs.js
+++ safedata/d3fs.js
@@ -11,8 +11,8 @@ var color;
 
 var count = 500;
 (function displayLinearColor( global ){
-for(let var i = 1; i < global.count; i++) {
-  let var band = document.createElement('div');
+for(var i = 1; i < global.count; i++) {
+  var band = document.createElement('div');
   band.style.background = global.color(i);
   band.style.width = '100%';
   band.style.height = '1px';
