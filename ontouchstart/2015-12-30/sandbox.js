fb7f6a398497f1ba0d293f33e15e53c9898b2cb6
Sam Liu <ontouchstart@gmail.com>
2015-12-30 09:11:55 -0500

    Update sandbox.js

--- ontouchstart/2015-12-30/sandbox.js
+++ ontouchstart/2015-12-30/sandbox.js
@@ -1,5 +1,7 @@
 // 2015-12-30
 src = 'https://raw.githubusercontent.com/bigdata-mindstorms/d3-playground/f38ff204e185e358edef35957fd470f8c4f14abf/lzyforever/2015-12-30/sandbox.js';
 d3.text(src, function (e, d) { 
-  console.log(d);
+  if(!e) {
+    eval(d);
+  }
 })
