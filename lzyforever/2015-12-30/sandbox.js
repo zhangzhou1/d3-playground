f38ff204e185e358edef35957fd470f8c4f14abf
lzyforever <261087685@qq.com>
2015-12-30 21:43:42 +0800

    Update sandbox.js

--- lzyforever/2015-12-30/sandbox.js
+++ lzyforever/2015-12-30/sandbox.js
@@ -1,3 +1,25 @@
+//alert("hello world!");
 
-//这个怎么玩？？？
-alert("hello world!");
+
+var dataset = [
+              {x : 20, y : 30, r : 10}, 
+              {x : 60, y : 30, r : 10}, 
+              {x : 80, y : 30, r : 10}
+    ];
+
+var svg = d3.select("body").append("svg");
+
+var circle = svg.selectAll(".myCircle")
+                .data(dataset)
+                .enter()
+                .append("circle")
+                .attr("class", "myCircle")
+                .attr("cx", function(d){
+                  return d.x;
+                })
+                .attr("cy", function(d){
+                  return d.y;
+                })
+                .attr("r", function(d){
+                  return d.r;
+                });
