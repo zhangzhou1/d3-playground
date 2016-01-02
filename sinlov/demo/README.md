4522e29bef86ff8a5f8ce38e968c94cc1590b977
sinlov <sinlovgmppt@gmail.com>
2016-01-02 21:00:20 +0800

    add append_ol_base.js and needed datas

---
+++ sinlov/demo/README.md
@@ -0,0 +1,5 @@
+# sinlov
+
+## demo
+
+[append_ol_base](https://bigdata-mindstorms.github.io/d3-playground/#https://bigdata-mindstorms.github.io/d3-playground/sinlov/demo/append_ol_base.js)
---
+++ sinlov/demo/append_ol_base.js
@@ -0,0 +1,26 @@
+ol = body.append('ol');
+src = 'https://bigdata-mindstorms.github.io/d3-playground/sinlov/demo/data.yaml';
+src_chinese = 'https://bigdata-mindstorms.github.io/d3-playground/sinlov/demo/data_chinese.yaml';
+
+function update(d) {
+  selection = ol.selectAll('li').data(d);
+  selection.enter().append('li');
+  selection.exit().remove();
+  selection.html(function (item) {
+      return item.name;
+    })
+    .style('background-color', function (item) {
+      return item.color;
+    });
+}
+
+function load(e, d) {
+  if(!e) {
+    update(js_yaml.load(d));
+  }
+}
+
+d3.text(src, load);
+setTimeout(function () {
+    d3.text(src_chinese, load);
+  }, 1000);
