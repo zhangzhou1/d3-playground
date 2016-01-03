0449fca1af3b5a038abd0255213abf7de09b45e1
ontouchstart <ontouchstart@gmail.com>
2016-01-03 17:21:56 -0500

    set global: body = d3.select('body');

--- ontouchstart/index.js
+++ ontouchstart/index.js
@@ -2,6 +2,7 @@ d3 = require('d3');
 marked = require('marked');
 js_yaml = require('js-yaml');
 src = location.hash.replace(/^#/, '') || 'main.js';
+body = d3.select('body');
 d3.text(src, function (e, d){
   if(!e) {
     eval(d);
